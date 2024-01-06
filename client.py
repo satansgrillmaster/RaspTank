a994ff2fcd38d305a068cd6d56ba592593bbc344
Matthias <47850805+satansgrillmaster@users.noreply.github.com>
2024-01-06 18:34:56 +0100

    add servo

--- client.py
+++ client.py
@@ -1,13 +1,34 @@
 import socket
 import json
-from server import LEDapp
+import LEDapp, servo
+
+SERVO_0 = 12
+SERVO_1 = 13
+SERVO_2 = 14
+SERVO_3 = 15
 
 
 class Client:
+
     def __init__(self, host='192.168.50.122', port=5000):
         self.host = host
         self.port = port
         self.client = None
+
+        self.servo_0_rotation = 0
+        self.is_first_step_servo_0 = True
+
+        self.servo_1_rotation = 0
+        self.is_first_step_servo_1 = True
+
+        self.servo_2_rotation = 0
+        self.is_first_step_servo_2 = True
+
+        self.servo_3_rotation = 0
+        self.is_first_step_servo_3 = True
+
+        self.servo_4_rotation = 0
+        self.is_first_step_servo_4 = True
 
     def start(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,19 +40,82 @@ class Client:
         return json_data
 
     def process_data(self, data, led):
+
         actions = {
             'w': (0, 255, 0),
             'a': (255, 0, 0),
             's': (0, 0, 255),
             'd': (255, 255, 255),
         }
-        key = data.get('key')
-        colors = actions.get(key, 'Unknown command')
-        led.colorWipe(*colors)
+        servo_actions = {
+            '1': {'chanel': SERVO_2, 'initial': 250, 'min': 90, 'final': 500, 'step': 5},
+            '2': {'chanel': SERVO_2, 'initial': 250, 'min': 90, 'final': 500, 'step': -5},
+            '3': {'chanel': SERVO_1, 'initial': 250, 'min': 90, 'final': 500, 'step': 5},
+            '4': {'chanel': SERVO_1, 'initial': 250, 'min': 90, 'final': 500, 'step': -5},
+        }
 
+        key = data.get('key')
+        is_first_step = data.get('is_first_step')
+
+        if key in actions:
+            colors = actions.get(key, 'Unknown command')
+            led.colorWipe(*colors)
+        elif key in servo_actions:
+            _servo = servo_actions.get(key)
+            self.move_servo(_servo, is_first_step)
 
     def stop(self):
         self.client.close()
+
+    def move_servo(self, _servo, is_first_step):
+
+        chanel = _servo.get('chanel')
+        initial = _servo.get('initial')
+        min = _servo.get('min')
+        final = _servo.get('final')
+        step = _servo.get('step')
+
+        rotation = 0
+
+        if is_first_step:
+            if chanel == 12:
+                self.servo_0_rotation = initial
+                rotation = self.servo_0_rotation
+            elif chanel == 13:
+                self.servo_1_rotation = initial
+                rotation = self.servo_1_rotation
+            elif chanel == 14:
+                self.servo_2_rotation = initial
+                rotation = self.servo_2_rotation
+            elif chanel == 15:
+                self.servo_3_rotation = initial
+                rotation = self.servo_3_rotation
+        else:
+            if chanel == 12:
+                self.servo_0_rotation += step
+                rotation = self.servo_0_rotation
+            elif chanel == 13:
+                self.servo_1_rotation += step
+                rotation = self.servo_1_rotation
+            elif chanel == 14:
+                self.servo_2_rotation += step
+                rotation = self.servo_2_rotation
+            elif chanel == 15:
+                self.servo_3_rotation += step
+                rotation = self.servo_3_rotation
+
+        if rotation < min:
+            rotation = initial
+            servo.pwm.set_pwm(chanel, 0, rotation)
+
+        elif rotation > final:
+            rotation = initial
+            servo.pwm.set_pwm(chanel, 0, rotation)
+
+        else:
+            rotation += step
+            servo.pwm.set_pwm(chanel, 0, rotation)
+
 
 if __name__ == '__main__':
     client = Client()
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import socket
 import json
+import time
+
 import keyboard
+
+SERVO_1_EVENTS = ['3', '4']
+SERVO_2_EVENTS = ['1', '2']
 
 class Server:
     def __init__(self, host='0.0.0.0', port=5000):
@@ -30,11 +35,24 @@ if __name__ == '__main__':
     server = Server()
     server.start()
 
+    used_events = []
+
     def send_keypress(event):
-        if event.name in ['w', 'a', 's', 'd']:
-            server.send_data({'key': event.name})
+        if event.name in ['w', 'a', 's', 'd', '1', '2', '3', '4']:
+            if event.name not in used_events:
+
+                if event.name in SERVO_1_EVENTS:
+                    for servo_event in SERVO_1_EVENTS:
+                        used_events.append(servo_event)
+                elif event.name in SERVO_2_EVENTS:
+                    for servo_event in SERVO_2_EVENTS:
+                        used_events.append(servo_event)
+
+                server.send_data({'key': event.name, 'is_first_step': True})
+            else:
+                server.send_data({'key': event.name, 'is_first_step': False})
+
 
     keyboard.on_press(send_keypress)
-
     while True:
         pass
